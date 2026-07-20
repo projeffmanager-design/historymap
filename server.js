662ef8fea05b08d82f1872072cd98c7eeb883d5f
고리만리지국 <projeffmanager@gmail.com>
2026-07-20 23:31:13 +0900

    3.8.0

--- server.js
+++ server.js
@@ -3209,7 +3209,7 @@ app.get('/api/castle', async (req, res) => {  // ← async 이미 있음
 
 // GET: 앱 버전 반환 (login.html 등 외부 페이지용)
 app.get('/api/app-version', (req, res) => {
-    res.json({ version: '3.7.18' });
+    res.json({ version: '3.8.0' });
 });
 
 // GET: 모든 장수 정보 반환
